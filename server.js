5db472423abd4fa2a88b035025c618ece6755bbe
Joshua Alexes Dignadice <joshuadignadice24@gmail.com>
2021-02-09 16:40:37 +0800

    initail commit

--- server.js
+++ server.js
@@ -37,9 +37,13 @@ mongoose.connect(URI, {
 })
 
 if(process.env.NODE_ENV === 'production'){
-    app.use(express.static('client/build'))
+    app.use(express.static(path.join(__dirname, "/client/build")))
     app.get('*', (req, res) => {
-        res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'))
+        res.sendFile(path.join(__dirname, "client", "build", "index.html"))
+    })
+}else{
+    app.get('/', (req, res) => {
+        res.send('API Running')
     })
 }
 
